4627457f054efc4c8ceb5b9d0d6e07dc2a351e11
elijah <elijahvarga@gmail.com>
2022-01-31 10:58:52 -0800

    Added button to start customizing player's puzzle, button takes player to new activity

---
+++ app/src/main/java/com/example/puzzler/ConfigurePuzzleActivity.kt
@@ -0,0 +1,11 @@
+package com.example.puzzler
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ConfigurePuzzleActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_configure_puzzle)
+    }
+}
--- app/src/main/java/com/example/puzzler/MainActivity.kt
+++ app/src/main/java/com/example/puzzler/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.puzzler
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val newPuzzle = findViewById<Button>(R.id.newPuzzleButton)
+        newPuzzle.setOnClickListener {
+            val intent = Intent(this,ConfigurePuzzleActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
